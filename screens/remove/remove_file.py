448e65b91e0e88547e49792286789519c2e579a9
EricLiu750501 <qazwsxedc123456789eric@gmail.com>
2025-01-08 21:00:07 +0800

    screen UI consistency

--- screens/remove/remove_file.py
+++ screens/remove/remove_file.py
@@ -1,6 +1,6 @@
 from utils.rm_files import *
 from screens.base_screen import BaseScreen
-from utils.constants import WINDOW_SIZE, FONT
+from utils.constants import WINDOW_SIZE, FONT_BOLD
 from utils.CvDrawText import CvDrawText
 from utils.drawing import draw_button
 import cv2
@@ -8,7 +8,7 @@ import cv2
 class  RemoveFileScreen(BaseScreen):
     def __init__(self, callback):
         super().__init__(callback)
-        self.font_path = FONT
+        self.font_path = FONT_BOLD
         self.title = "Remove File"
         self.button_titles = ["Reset Created Gestures", "Reset Created Jutsu", "Remove Temp Naruto Gestures", "Remove ALL FILES"]
         self.button_functions = [remove_gestures, remove_jutsu, remove_temp, remove_all]
@@ -21,7 +21,7 @@ class  RemoveFileScreen(BaseScreen):
         self.gap = 15
         self.total_height = self.button_height 
 
-        self.start_y = self.total_height // 2
+        self.start_y = self.total_height // 2 + 100
         self.x = WINDOW_SIZE[0] // 2
         for i, button_name in enumerate(self.button_titles):
             y1 = self.start_y + i * (self.button_height + self.gap)
@@ -35,7 +35,14 @@ class  RemoveFileScreen(BaseScreen):
 
     def draw(self, frame):
         
-
+        CvDrawText.puttext(
+            frame,
+            "刪除使用者資料",
+            (self.x , self.start_y - 100),
+            self.font_path,
+            50,
+            (0, 0, 0),
+        )
         
         for i, button_name in enumerate(self.button_titles):
             y1 = self.start_y + i * (self.button_height + self.gap)
@@ -51,19 +58,21 @@ class  RemoveFileScreen(BaseScreen):
                 hover=hover,
             )
         # Draw buttons
-        back_x, back_y = 950, WINDOW_SIZE[1] - 100
+        back_x, back_y = 1100, WINDOW_SIZE[1] - 100
+        back_button_width =150
+        back_button_height = 60
         cv2.rectangle(
-            frame, (back_x, back_y), (back_x + 200, back_y + 50), (0, 0, 255), -1
+            frame, (back_x, back_y), (back_x + back_button_width, back_y + back_button_height), (40, 40, 245), -1
         )
         CvDrawText.puttext(
             frame,
             "返回",
-            (back_x + 70, back_y + 10),
+            (back_x + 45, back_y + 10),
             self.font_path,
             30,
             (255, 255, 255),
         )
-        self.button_areas.append((back_x, back_y, back_x + 200, back_y + 50, "back"))
+        self.button_areas.append((back_x, back_y, back_x + back_button_width, back_y + back_button_height, "back"))
 
     def handle_click(self, x, y):
         for x1, y1, x2, y2, action in self.button_areas:
