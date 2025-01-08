a77d423ecb97894560b3f5061d212f4928d90b07
EricLiu750501 <qazwsxedc123456789eric@gmail.com>
2025-01-08 19:30:06 +0800

    reomve redundency print

--- utils/rm_files.py
+++ utils/rm_files.py
@@ -9,7 +9,6 @@ def reset_created_gestures():
     with open(file_path, 'w') as file:
         json.dump([], file)
     print(f'Reset {file_path} to an empty list.')
-    reset_created_jutsu() # 重置手勢時，也重置忍術
 
 
 def reset_created_jutsu():
@@ -35,9 +34,7 @@ def remove_gesture_files():
     folder_path = 'assets/images/'
     for filename in os.listdir(folder_path):
         if filename.startswith('gesture_'):
-                print(filename)
                 gesture_number = int(filename.split('_')[1] [:-4]) # .jpg suffix
-                print(gesture_number)
                 if gesture_number > 12:
                     file_path = os.path.join(folder_path, filename)
                     if os.path.isfile(file_path):
